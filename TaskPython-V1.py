dfab7d3ad92a768106cc3863194cc9a9f4b522ff
A.Apanovich <a.apanovich@mail.ru>
2021-12-11 12:43:36 +0300

    Inset file in Project

---
+++ TaskPython-V1.py
@@ -0,0 +1,10 @@
+## about Python
+
+print("Хочешь узнать о Python?")
+st_about = input("Input Y or N \n")
+
+if st_about == 'Y' or st_about == 'y':
+    print('Python — это активно развивающийся скриптовый язык, который используют для решения большого объема самых разноплановых проблем и задач.')
+
+else:
+    print('Жаль, что вам не интересно. Посетите сайт: https://www.python.org/')
