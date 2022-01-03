a4e9a6edde83cadf19c3e798b80f8a4bd8df9228
A.Apanovich <a.apanovich@mail.ru>
2022-01-04 00:49:48 +0300

    Adder Lesson 5

--- taskWrk/TaskPython-V5-4.py
+++ taskWrk/TaskPython-V5-4.py
@@ -1,5 +1,114 @@
 # *** Обработка ошибок: Exception
 
+import math
+from typing import final
 
+array = [9, 4, -25, 64, 49, 81, -16]
+
+# Выполнение через if-elif-else
+"""
+for num in array:
+    if num > 0:
+        print(f"Корень числа {num}: {math.sqrt(num)}")
+    
+    else:
+        print("Отрицательное цисло")
+"""
+
+# Выполнение через try except else finally
+for num in array:
+    
+    # Код, который может вызвать ошибку
+    try:
+        print(f"Корень числа {num}: {math.sqrt(num)}")
+    
+    # Обработчик ощибки
+    except:
+        print(f"Отрицательное цисло: {num}")
+    
+    # *** Опционально
+    
+    # Если ошибка не произошла
+    else:
+        print("Положительное число: ", abs(num))
+    
+    # Выполнять, была ли вызвана ошибка
+    finally:
+        if num < 0:
+            print(f"Корень отрицательного числа {num}: по модулю {math.sqrt(abs(num))}")
+            
+
+a = input('Введите первое число: ')
+b = input('Введите первое число: ')
+
+# Проверка на число
+def is_numerable(s):
+    # Проверяем строку
+    try:
+        int(s)
+    
+    # Если ошибка
+    except:
+        return False
+        
+    # Еслм нет ошибки    
+    else:
+        return True
+        
+# Проверяем число "a"
+if is_numerable(a):
+    print('Число')
+
+else:
+    print('Не число')
+
+# Проверяем число "b"
+if is_numerable(b):
+    print('Число')
+
+else:
+    print('Не число')
+
+
+try:
+    a = int(a)
+    b = int(b)
+    print(a/b)
+
+except ValueError as VE:
+    #print('Error')
+    print("Не число", ValueError)
+    print(VE, "Тип ошибки: ", type(VE))   
+ 
+except ArithmeticError as AE:
+    #print("Error")
+    print("Арифметическая", ArithmeticError)
+    print(AE, "Тип ошибки: ", type(AE)) 
+
+"""
+except ZeroDivisionError as ZE:
+    print("Деление на ноль", ZeroDivisionError)
+    print(ZE, "Тип ошибки: ", type(ZE))
+"""
+
+# *** Запись в файл
+"""
+a = input('Введите цисло:')            
+
+try:
+    f = open('resource/file-5.txt')
+    a = int(a)
+    
+except:
+    print('Error')
+    
+else:
+    f.write(a)
+
+finally:
+    a = 0
+    f.write(a)
+    f.close()
+"""
 
 # End
--- taskWrk/TaskPython-V5-5.py
+++ taskWrk/TaskPython-V5-5.py
@@ -1,5 +1,36 @@
 # *** Вызываемые исключения: Raise
 
+class ZeroAdditionError(ValueError):
+    pass
+    
 
+a = input('Введите число "a": ')
+b = input('Введите число "b": ')
 
+def add_to(a, b):
+    if a == 0:
+        # Если a = 0 
+        a = 100
+        
+    if b == 0:
+        # Сообщить об ошибке
+        raise ZeroAdditionError('zero addition is not supported')
+        #raise ZeroAdditionError # - Без комментария
+        #raise ValueError('zero addition is not supported')
+    return a * b
+
+# *** 
+try:
+    a = int(a)
+    b = int(b)
+    print(add_to(a, b))
+
+except ValueError as VE:
+    print(ValueError),
+    print("сообщение об ошибке: ", VE, "\n"+"тип ошибки: ", type(VE))
+    # Если b = 0 и получаем ошибку, то b = 1  
+    b = int(1)
+    print(add_to(a, b))
+
+    
 # End
