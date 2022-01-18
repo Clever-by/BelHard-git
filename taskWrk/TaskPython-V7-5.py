453214728d19071f3088645d75efbdbd336ce062
A.Apanovich <a.apanovich@mail.ru>
2022-01-18 16:09:18 +0300

    Lesson 7

--- taskWrk/TaskPython-V7-5.py
+++ taskWrk/TaskPython-V7-5.py
@@ -2,20 +2,61 @@
 
 # Менеджеры контекста
 
-with open('taskWrk\\resource\p.txt') as f:
+"""
+with open('taskWrk\\resource\list.txt') as f:
     print(f.read())
+"""
 
+# Обработка через исключения
 
-# versus
 """
 try:
-    f = open(‘list.txt’)
+    f = open('taskWrk\\resource\list.txt')
     print(f.read())
 except:
     # обработка исключений
 finally:
     f.close()
 """
+
+class OpenidFile:
+    
+    def __init__(self, textfile):
+        self.textfile = textfile
+    
+    def __enter__(self):
+        self.file = open(self.textfile)
+        return self.file
+    
+    def __exit__(self, *args):
+        print(args)
+        self.file.close()
         
-        
+try:
+    with OpenidFile('taskWrk\\resource\list.txt') as f:
+        print(f.read())
+
+except:
+    print("Close File, because error: ")
+    
+
+from contextlib import suppress
+
+try:
+    1/0
+
+except ValueError as VE:
+    #pass
+    Err = VE
+    print(Err, type(VE))
+
+with suppress(ZeroDivisionError):
+    1/0
+    
+
+from contextlib import redirect_stdout
+
+with redirect_stdout(open('taskWrk\\resource\stdout.txt', 'a')):
+    print('Context Managers')
+
 # End
