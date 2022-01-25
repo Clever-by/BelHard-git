f7905c0ff4b9bab36563a0692afd6c2fee239b2d
A.Apanovich <a.apanovich@mail.ru>
2022-01-26 01:32:39 +0300

    chanded Lesson 8

--- homeWork/HomeWrkPython-V8-2.py
+++ homeWork/HomeWrkPython-V8-2.py
@@ -1,22 +1,13 @@
-# Функция Factorial:
-
-
-def f(n):
-    res = 1
-    for num in range(1, n+1):
-        res *= num
-
-    return res
-
-
-print(f'Facrotial: {f(5)}')
-
-
-# Расчет факториалф через рекурсию
+# Расчет факториала
 
 def factorial(n):
-    #if n == 1:
-        #return n
-    return n * factorial(n-1) if n > 1 else 1
 
-print(f'Facrotial: {factorial(6)}')
+    f = 1
+    if n >= 1:
+        f = n * factorial(n-1)
+    
+    return f
+    #return n * factorial(n-1) if n > 1 else 1
+
+
+print(f'Facrotial: {factorial(5)}')
