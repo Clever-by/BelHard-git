0bf84a659a3c247e00c45ac90359f7b5e9f2bd81
A.Apanovich <a.apanovich@mail.ru>
2022-02-04 13:22:13 +0300

    Cnanged practic №8-1

--- homeWork/HomeWrkPython-V8-1.py
+++ homeWork/HomeWrkPython-V8-1.py
@@ -39,64 +39,112 @@ class Line:
             self.point_end = point_end
         
     def getlenght(self):
-        
+        print('Lenght:')
         return float(self.point_start + self.point_end)
-        
+    
+    def setlenght(self, point_new_start, point_new_end):
+        if point_new_start is None:
+            self.point_start = Point().y
+        else:
+            self.point_start = point_new_start
+        if point_new_end is None:
+            self.point_end = Point().y
+        else:
+            self.point_end = point_new_end
+        #print('Lenght:')
+        return float(point_new_start + point_new_end)
+
 
 #Объявляем Class Share
 class Share:
     
-    def __init__(self, side_a, side_b):
-        self.side_a = side_a
-        self.side_b = side_b
+    a = 9
+    b = 16
+    c = 25
+    
+    def __init__(self, side_a = a, side_b = b):
+        if side_a is None or side_a == '':
+            self.side_a = self.a
+        else:
+            self.side_a = side_a
+        
+        if side_b is None or side_b == '':
+            self.side_b = self.b
+        else:
+            self.side_b = side_b
     
     def getarea(self):
+        #print('Area:')
         return float(self.side_a * self.side_b)
 
     def getperimeter(self):
+        #print('Perimeter:')
         return float((self.side_a + self.side_b)*2)
 
 # Объявляем Class Rect и наследуем от Класса Share
 class Rect(Share):
     
-    def setRect(self):
-        side = Line().getlenght()
-        #print(f'{side}')
-        self.side_a = Line().getlenght()
-        self.side_b = Line().getlenght()
-        return side
+    def show(self):
+        area = super().getarea()
+        perimeter = super().getperimeter()
+        return f'"Площадь rect"={area}, "Периметр rect"={perimeter}'
+
+    def setRect(self, point_a1, point_b1, point_a2, point_b2):
+        self.side_a = Line().setlenght(point_a1, point_a2)
+        self.side_b = Line().setlenght(point_b1, point_b2)
+        return f'side_a={self.side_a}, side_b={self.side_b}'
+
+
+# Объявляем Class Square и наследуем от Классов Share, Line 
+class Square(Share, Line):
     
     def show(self):
         area = super().getarea()
         perimeter = super().getperimeter()
-        return f'Площадь={area}, Периметр={perimeter}'
+        return f'"Площадь square"={area}, "Периметр square"={perimeter}'
 
-point = Point(10,None)
+    def setSquare(self, point_a1, point_b1, point_a2, point_b2):
+        
+        self.side_a = super().setlenght(point_a1, point_a2)
+        self.side_b = super().setlenght(point_b1, point_b2)
+        return f'side_a={self.side_a}, side_b={self.side_b}'
+
+
+# Объявляем Class Square и наследуем от Классов Share
+class Cube(Square):
+    
+    def setCube(self, point_c1, point_c2):
+        self.side_c = super().setlenght(point_c1, point_c2)
+        rez = self.side_a * self.side_b * self.side_c
+        return f'"Объем cube"={rez}'
+    
+#point = Point()
+#print(Point.mro())
 #print(point.getpoint())
 
-
-line = Line()
+#line = Line()
+#print(Line.mro())
+#print(line.getlenght())
+#print(line.setlenght(16,25))
 #print(line.getlenght())
 
-
-#share = Share(9,16)
+#share = Share()
+#print(Share.mro())
 #print(share.getarea())
 #print(share.getperimeter())
 
-rect = Rect(3,4)
-#rect.setRect()
-#print(rect.setRect())
-print(rect.show())
+#rect = Rect(11,12)
+#print(Rect.mro())
+#print(rect.show())
+#print(rect.setRect(3,5,3,5))
+#print(rect.show())
 
-#cube = Cube(3,4,5)
-#print(cube.getvolume())
+#square = Square()
+#print(Square.mro())
+#print(square.show())
+#print(square.setSquare(7,9,7,9))
+#print(square.show())
 
-        
-#square = Square(3,None,5)
-#print(square.getarea())
-#print(square.getperimeter())
-
-
-#rect = Rect()
-#print(rect.getarea())
-#print(rect.getperimeter())
+cube = Cube(3, 5)
+print(Cube.mro())
+print(cube.setCube(2,2))
